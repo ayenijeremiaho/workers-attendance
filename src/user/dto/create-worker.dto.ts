a58ef65899de8583a3c536dd54fcb6e53684cdf5
Jeremiah Ayeni <jayeni@getcarbon.co>
2025-06-01 17:31:59 +0100

    enforce validation  when creating workers

--- src/user/dto/create-worker.dto.ts
+++ src/user/dto/create-worker.dto.ts
@@ -1,4 +1,10 @@
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import {
+  IsBoolean,
+  IsEnum,
+  IsNotEmpty,
+  IsUUID,
+  Matches,
+} from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 import { MaritalStatusEnum } from '../enums/marital-status.enum';
 
@@ -7,33 +13,33 @@ export class CreateWorkerDto extends CreateUserDto {
   @IsUUID('4', { message: 'invalid departmentId' })
   departmentId: string;
 
-  // @Matches(/^\d{4}-\d{2}-\d{2}$/, {
-  //   message: 'yearBaptized must be in the format YYYY-MM-DD',
-  // })
+  @Matches(/^\d{4}-\d{2}-\d{2}$/, {
+    message: 'yearBaptized must be in the format YYYY-MM-DD',
+  })
   yearBaptized: string;
 
-  // @Matches(/^\d{4}-\d{2}-\d{2}$/, {
-  //   message: 'yearBornAgain must be in the format YYYY-MM-DD',
-  // })
+  @Matches(/^\d{4}-\d{2}-\d{2}$/, {
+    message: 'yearBornAgain must be in the format YYYY-MM-DD',
+  })
   yearBornAgain: string;
 
-  // @IsNotEmpty()
+  @IsNotEmpty()
   profession: string;
 
-  // @IsNotEmpty()
-  // @IsEnum(MaritalStatusEnum, {
-  //   message: 'maritalStatus must be a valid enum value',
-  // })
+  @IsNotEmpty()
+  @IsEnum(MaritalStatusEnum, {
+    message: 'maritalStatus must be a valid enum value',
+  })
   maritalStatus: MaritalStatusEnum;
 
-  // @Matches(/^\d{4}-\d{2}-\d{2}$/, {
-  //   message: 'yearJoinedWorkforce must be in the format YYYY-MM-DD',
-  // })
+  @Matches(/^\d{4}-\d{2}-\d{2}$/, {
+    message: 'yearJoinedWorkforce must be in the format YYYY-MM-DD',
+  })
   yearJoinedWorkforce: string;
 
-  // @IsBoolean()
+  @IsBoolean()
   completedSOD: boolean;
 
-  // @IsBoolean()
+  @IsBoolean()
   completedBibleCollege: boolean;
 }
